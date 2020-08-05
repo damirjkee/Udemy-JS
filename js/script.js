2e769d2328f833e601b8ce61b65e991b1cd83fa2
Damir <damirka.mukanov@gmail.com>
2020-08-05 13:53:19 +0600

    Udemy JS. Условия

--- js/script.js
+++ js/script.js
@@ -1,21 +1,37 @@
 "use strict";
 
-const number0fFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
+if(4 == 9){
+    console.log('ok!');
+} else{
+    console.log('Error');
+}
 
-const personalMovieDB = {
-    count: number0fFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
 
-const a = prompt('Один из последних просмотренных фильмов?', ''),
-      b = prompt ('На сколько оцените его?'),
-      c = prompt('Один из последних просмотренных фильмов?', ''),
-      d = prompt ('На сколько оцените его?');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
 
-console.log(personalMovieDB);
+// if(num < 49) {
+//     console.log('Error');
+// } else if (num > 100){
+//     console.log('Много');
+// } else{
+//     console.log('Ok!');
+// }
+
+// (num === 50) ? console.log('Ok!') : console.log('Error');
+
+const num = 50;
+
+switch (num) {
+    case 49:
+        console.log('Неверно');
+        break;
+    case 100:
+        console.log('Неверно');
+        break;
+    case 50:
+        console.log('В точку!');
+        break;
+    default:
+        console.log('Не в этот раз');
+        break;
+}
