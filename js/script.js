148321484117561cb1b3eb2f81cb363d04d2fa63
Damir <damirka.mukanov@gmail.com>
2020-08-05 17:50:02 +0600

    Udemy JS. Практика ч2. Применяем условия и циклы

--- js/script.js
+++ js/script.js
@@ -1,22 +1,38 @@
 "use strict";
 
-let num = 50;
+const number0fFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
 
-// while (num < 55) {
-//     console.log(num);
-//     num++;
-// }
+const personalMovieDB = {
+    count: number0fFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
 
-// do {
-//     console.log(num);
-//     num++;
-// }
-// while(num < 55);
+for (let i = 0; i < 2; i++){
 
-for(let i = 1; i < 10; i++){
-    if (i === 6){
-        // break;
-        continue;
+    const a = prompt('Один из последних просмотренных фильмов?', ''),
+          b = prompt('На сколько оцените его?', '');
+
+    if(a != null && b != null && a != '' && b != '' && a.length < 50){
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else{
+        console.log('error');
+        i--;
     }
-    console.log(i);
 }
+
+
+if (personalMovieDB.count < 10){
+    console.log('Просмотрено довольно мало фильмов');
+} else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
+    console.log('Вы классический зритель');
+} else if (personalMovieDB.count >= 30){
+    console.log('Вы киноман');
+} else {
+    console.log('Произошла')
+}
+
+console.log(personalMovieDB);
