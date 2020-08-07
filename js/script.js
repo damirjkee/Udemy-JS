64bd8b5dd8772a5ed8f1b63bfb65afe62a5344cc
Damir <damirka.mukanov@gmail.com>
2020-08-07 17:03:10 +0600

    17. Udemy JS. Методы и свойства строк и чисел

--- js/script.js
+++ js/script.js
@@ -1,38 +1,58 @@
 "use strict";
 
-const number0fFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
+// const number0fFilms = prompt('Сколько фильмов вы уже посмотрели?');
 
-const personalMovieDB = {
-    count: number0fFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+// const personalMovieDB = {
+//     count: number0fFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+    
+// let retry = 0;
 
-for (let i = 0; i < 2; i++){
+// while (retry < 2) {
+//     retry++;
 
-    const a = prompt('Один из последних просмотренных фильмов?', ''),
-          b = prompt('На сколько оцените его?', '');
+//     const a = prompt('Один из последних просмотренных фильмов?', ''),
+//           b = prompt('На сколько оцените его?', '');
+          
+    
 
-    if(a != null && b != null && a != '' && b != '' && a.length < 50){
-        personalMovieDB.movies[a] = b;
-        console.log('done');
-    } else{
-        console.log('error');
-        i--;
-    }
-}
+//     if (a != null && b != null && a != ''&& b != '' && a.length < 50 && b.length < 50) {
+//         personalMovieDB.movies [a] = b;
+//         console.log('Done');
+//     } else {
+//         console.log('Error');
+//     }
+// }
 
+// if (personalMovieDB.count < 10) {
+//     console.log('Просмотрено довольно мало фильмов');
+// } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+//     console.log('Вы классический зритель');
+// } else if (personalMovieDB.count >= 30) {
+//     console.log('Вы киноман');
+// } else {
+//     console('Произошла ошибка');
+// }
 
-if (personalMovieDB.count < 10){
-    console.log('Просмотрено довольно мало фильмов');
-} else if(personalMovieDB.count >= 10 && personalMovieDB.count < 30){
-    console.log('Вы классический зритель');
-} else if (personalMovieDB.count >= 30){
-    console.log('Вы киноман');
-} else {
-    console.log('Произошла')
-}
+// console.log(personalMovieDB);
 
-console.log(personalMovieDB);
+const str = 'test';
+
+console.log(str);
+console.log(str);
+
+const fruit = 'Some fruit';
+
+console.log(fruit.indexOf('q'));
+
+const logg = 'Hello world';
+
+// // console.log(logg.slice(6, 11));
+
+// console.log(logg.substring(6, 11));
+
+console.log(logg.substr(6, 5));
